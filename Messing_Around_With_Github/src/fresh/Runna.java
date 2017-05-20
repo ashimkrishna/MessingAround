f21dc6bde5c3ef67905362c86ef6fc67ef5bfdc8
Ashim Krishna <ashim.krishna94@gmail.com>
2017-05-19 21:19:54 -0400

    initial commit

---
+++ Messing_Around_With_Github/src/fresh/Runna.java
@@ -0,0 +1,16 @@
+package fresh;
+
+public class Runna {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Apple apple = new Apple();
+		
+		Orange orange = new Orange();
+		
+		System.out.println("This is the orange and apple: " + apple + orange);
+
+	}
+
+}
